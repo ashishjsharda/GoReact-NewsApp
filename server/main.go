cfa16a428b70ea0541e3e70d60e25264d5b0f0e3
Ashish Sharda <ashishjsharda@gmail.com>
2023-07-08 16:54:44 -0500

    fetch top 5 articles

--- server/main.go
+++ server/main.go
@@ -1,8 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type Article struct {
 	Title string `json:"title"`
-	URL   string `json:"url"`
+	Url   string `json:"url"`
 }
 
 type Response struct {
@@ -10,5 +15,14 @@ type Response struct {
 }
 
 func main() {
-	
+	http.HandleFunc("/top-articles", func(w http.ResponseWriter, r *http.Request) {
+		resp, _ := http.Get("https://gnews.io/api/v4/top-headlines?token=YOUR_API_KEY&lang=en&max=5")
+
+		var response Response
+		json.NewDecoder(resp.Body).Decode(&response)
+
+		json.NewEncoder(w).Encode(response.Articles)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
